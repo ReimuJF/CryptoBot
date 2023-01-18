3e882e326544e19f8e05ea3c93219b552c742e2a
ReimuJF <slavalang@gmail.com>
2023-01-18 06:52:35 +0500

    1801
    little optimize

--- main.py
+++ main.py
@@ -34,6 +34,15 @@ DATA = {
 }
 
 
+def get_currency_nicehash():
+    return requests.get("https://api2.nicehash.com/exchange/api/v2/info/prices").json()
+
+
+def get_currency_binance():
+    return requests.post("https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search",
+                         headers=HEADERS, json=DATA).json()['data'][0]['adv']['price']
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Enter your BTC balance: ')
@@ -50,8 +59,7 @@ def help(message):
 
 @bot.message_handler(commands=['btc'])
 def btc_price(message):
-    data_s = requests.get("https://api2.nicehash.com/exchange/api/v2/info/prices").json()
-    btc_prc = data_s['BTCUSDT']
+    btc_prc = get_currency_nicehash()['BTCUSDT']
     bot.send_message(message.chat.id, f'BTC price in USD {btc_prc}')
 
 
@@ -67,29 +75,26 @@ def buttons(message):
 @bot.message_handler(commands=['Nicehash', 'Binance'])
 def ref_link_one(message):
     markup = types.InlineKeyboardMarkup()
-    site_names = {'/Nicehash': ("Nicehash", "https://www.nicehash.com/my/mining/rigs"),
-                   "/Binance": ("Binance", "https://www.binance.com/")}
+    site_names = { '/Nicehash': ("Nicehash", "https://www.nicehash.com/my/mining/rigs"),
+                   "/Binance": ("Binance", "https://www.binance.com/") }
     markup.add(types.InlineKeyboardButton(site_names[message.text][0], url=site_names[message.text][1]))
     bot.send_message(message.chat.id, 'You welcome', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def get_user_text(message):
-    data_b = requests.post("https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search",
-                           headers=HEADERS, json=DATA).json()
-    data_s = requests.get("https://api2.nicehash.com/exchange/api/v2/info/prices").json()
-    LTC_CUR = data_s["LTCBTC"]
-    USDT_CUR = data_s["LTCUSDT"]
     try:
         btc_balance = float(message.text.replace(',', '.', 1))
     except ValueError:
         return bot.send_message(message.chat.id, 'Please enter numbers only')
-    ltc_balance = ((btc_balance / LTC_CUR) - ((btc_balance / LTC_CUR) * 0.004)) - 0.00002
-    p2p_usdt_rub = float(data_b['data'][0]['adv']['price'])
-    rub_balance = ltc_balance * USDT_CUR * p2p_usdt_rub
+    ltc_cur = get_currency_nicehash()["LTCBTC"]
+    usdt_cur = get_currency_nicehash()["LTCUSDT"]
+    ltc_balance = ((btc_balance / ltc_cur) - ((btc_balance / ltc_cur) * 0.004)) - 0.00002
+    p2p_usdt_rub = float(get_currency_binance())
+    rub_balance = ltc_balance * usdt_cur * p2p_usdt_rub
     bot.send_message(message.chat.id,
-                     f"LTC to BTC = {LTC_CUR}\nLTC Balance = {ltc_balance:.5f}\n"
-                     f"LTC to USD = {USDT_CUR}\nP2P USDT to RUB = {p2p_usdt_rub}\nYour income = {rub_balance:.2f} RUB")
+                     f"LTC to BTC = {ltc_cur}\nLTC Balance = {ltc_balance:.5f}\n"
+                     f"LTC to USD = {usdt_cur}\nP2P USDT to RUB = {p2p_usdt_rub}\nYour income = {rub_balance:.2f} RUB")
 
 
 bot.polling(none_stop=True)
